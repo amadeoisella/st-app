d6809d67ccc77e3063393eefe723f9986e1b6aff
amadeoisella <aisellamk@gmail.com>
2022-06-08 16:08:29 -0300

    connection mdb

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
+const exp = require("constants");
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+mongoose
+  .connect(
+    `mongodb+srv://pelaisella:${process.env.MONGO_DB_PASS}@development.tq5np.mongodb.net/st-app?retryWrites=true&w=majority`
+  )
+  .then(result => {
+    app.listen(PORT, () => {
+      console.log(`Listen on ${PORT}`);
+    });
+    console.log("Conexion exitosa a BBDD");
+  })
+  .catch(err => console.log(err));
+
+const productSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: Number,
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+app.use(express.json());
+
+app.post("/api/v1/products", (req, res) => {
+  const newProduct = new Product(req.body);
+
+  newProduct
+    .save()
+    .then(result => {
+      res.status(201).json({ ok: true });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
-const PORT = process.env.PORT || 4000;
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-  console.log(`Listen on ${PORT}`);
-});
+const PORT = process.env.PORT;
